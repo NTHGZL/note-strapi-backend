8c2f893f36c3de6ad976a341285ab94ef7565e63
Nathan Gonzalez <n.gonzalez@cavavin.fr>
2021-02-03 12:08:25 +0100

    Add Notes

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,17 +4,17 @@ module.exports = ({ env }) => ({
       default: {
         connector: 'mongoose',
         settings: {
-          uri: env('DATABASE_URI')
-          // host: env('DATABASE_HOST', 'strapi-cluster.zfefg.mongodb.net'),
-          // srv: env.bool('DATABASE_SRV', true),
-          // port: env.int('DATABASE_PORT', 27017),
-          // database: env('DATABASE_NAME', 'strapi-backend'),
-          // username: env('DATABASE_USERNAME', 'strapi'),
-          // password: env('DATABASE_PASSWORD', 'gw8BUM9X3EwCkVf'),
+        //   uri: env('DATABASE_URI')
+          host: env('DATABASE_HOST', 'strapi-cluster.zfefg.mongodb.net'),
+          srv: env.bool('DATABASE_SRV', true),
+          port: env.int('DATABASE_PORT', 27017),
+          database: env('DATABASE_NAME', 'strapi-backend'),
+          username: env('DATABASE_USERNAME', 'strapi'),
+          password: env('DATABASE_PASSWORD', 'gw8BUM9X3EwCkVf'),
         },
         options: {
-          // authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-          ssl: true//env.bool('DATABASE_SSL', true),
+          authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+          ssl : env.bool('DATABASE_SSL', true),
         },
       },
     },
